83271ba34216193c005fe4948bad051912acd939
Susan Tan <ssusantan9@gmail.com>
2019-06-04 17:24:06 -0400

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,16 @@
 // Code CoordinatesButton Component Here
+import React from 'react'
+
+export default class CoordinatesButton extends React.Component{
+    
+    handle = (event) => {
+        console.log(event.target)
+        this.props.onReceiveCoordinates([event.clientX, event.clientY])
+    }
+
+    render(){
+        return(
+            <button onClick = {this.handle}></button>
+        )
+    }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,19 @@
 // Code DelayedButton Component Here
+import React from "react";
+
+export default class DelayedButton extends React.Component {
+
+  handleClick = (event) => {
+    event.persist();
+    //to access the event properties in an asynchronous way, you should call event.persist() on the event, which will remove the synthetic event from the pool and allow references to the event to be retained by user code.
+    setTimeout(() => {
+      this.props.onDelayedClick(event)
+    }, this.props.delay);
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}/>
+    )
+  }
+}
